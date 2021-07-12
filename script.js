49bfd38f41a277c13f95cb330b538ee367527381
Brandon Foreman <71762399+foreman23@users.noreply.github.com>
2021-07-12 02:29:49 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 // Request - What do we want to do?
 // Response - What was actually done?
 
+
+
+// Contact form validation script
+let form = document.querySelector('.needs-validation')
+
+form.addEventListener('submit', function(event)  {
+    if (form.checkValidity() === false) {
+        event.preventDefault()
+        event.stopPropagation()
+    }
+    form.classList.add('was-validated')
+})
